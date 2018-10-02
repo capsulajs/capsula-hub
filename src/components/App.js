9498df0ad2b03465e9f544db8b697da883a93548
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2018-10-02 16:04:24 +0300

    add styles reset

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { getPersistor } from '@rematch/persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
+import styled from 'styled-components';
 import AppRouter from './Router';
 import store from '../store';
 import '../styles/App.css';
@@ -14,12 +15,25 @@ type State = {};
 
 const persistor = getPersistor();
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Header = styled.div`
+  background: #f7f7f7;
+  height: 44px;
+`;
+
 class App extends Component<Props, State> {
   render() {
     return (
       <PersistGate persistor={persistor}>
         <Provider store={store}>
-          <AppRouter />
+          <Container>
+            <Header>Header</Header>
+            <AppRouter />
+          </Container>
         </Provider>
       </PersistGate>
     );
