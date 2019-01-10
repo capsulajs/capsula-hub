5dc38c8405869123d70e783b95d3193cdbc34a42
MikeSquall <michael.arbib@om2.com>
2019-01-10 17:13:05 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+console.log('Hello from tsx!');
+
+ReactDOM.render(
+  <p>Hello</p>,
+document.getElementById('root'),
+);
+{
+  "compilerOptions": {
+    "jsx": "react"
+  }
+}
