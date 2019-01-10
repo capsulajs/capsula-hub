c877470582ba3c2959c2232ccb36be0b3b6d1aeb
MikeSquall <michael.arbib@om2.com>
2019-01-10 18:00:57 +0200

    Added travis configuration

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom'
 console.log('Hello from tsx!');
 
 ReactDOM.render(
-  <p>Hello</p>,
-document.getElementById('root'),
+  <div>
+    <p>Hello</p>
+  </div>,
+  document.getElementById('root'),
 );
-{
-  "compilerOptions": {
-    "jsx": "react"
-  }
-}
