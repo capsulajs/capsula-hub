8e5010134a9f6aa28130bee8d11c2edf0d969a41
MikeSquall <michael.arbib@om2.com>
2019-01-14 09:58:59 +0200

    Fixed the CLI run

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+const program = require('commander');
 
 program
   .version('0.0.1')
@@ -9,7 +9,14 @@ program
   .action(() => {
     const app = require('express')();
     const Bundler = require('parcel-bundler');
-    const bundler = new Bundler('src/index.html');
+    const Path = require('path');
+
+    const entryFiles = Path.join(__dirname, './index.html');
+    const options = {
+      outDir: '../dist',
+      outFile: 'index.html'
+    };
+    const bundler = new Bundler(entryFiles, options);
 
     app.use(bundler.middleware());
 
