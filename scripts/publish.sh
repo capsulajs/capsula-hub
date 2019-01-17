4d1de2776fc2de6bbf73edb61986507128c1a987
MikeSquall <michael.arbib@om2.com>
2019-01-17 12:54:37 +0200

    Tell travis when/how to deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 
-if [ ! "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if ! [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     VERSION=$(npm version | grep @ | sed -re "s/'.*': '(.*)',?/\1/g")
     BRANCH_NAME=$(echo $TRAVIS_PULL_REQUEST_BRANCH | sed "s/[/]/_/g")
-    npm version $VERSION-$BRANCH_NAME
+    TIMESTAMP=$(date +"%s")
+    npm version $VERSION-$BRANCH_NAME-$TIMESTAMP
     npm publish --tag pull-request
 fi
