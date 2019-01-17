ffe4464bfc870cd154c90bc57f341fbaec0eee24
MikeSquall <michael.arbib@om2.com>
2019-01-17 13:17:37 +0200

    test npm deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,11 @@ if ! [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     VERSION=$(npm version | grep @ | sed -re "s/'.*': '(.*)',?/\1/g")
     BRANCH_NAME=$(echo $TRAVIS_PULL_REQUEST_BRANCH | sed "s/[/]/_/g")
     TIMESTAMP=$(date +"%s")
+    echo "--------------------------------------------"
+    echo "|       Deploying on npm registry          |"
+    echo "--------------------------------------------"
     npm version $VERSION-$BRANCH_NAME-$TIMESTAMP
+    echo "npm version: " && [[ "$?" == 0 ]] && echo "Succeed" || echo "Failed"
     npm publish --tag pull-request
+    echo "npm publish: " && [[ "$?" == 0 ]] && echo "Succeed" || echo "Failed"
 fi
