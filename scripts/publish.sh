2d017792aab15d37f0fd9c03d824234182575859
MikeSquall <michael.arbib@om2.com>
2019-01-20 11:30:13 +0200

    fix npm deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 
 if ! [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    VERSION=$(npm version | grep @ | sed -re "s/'.*': '(.*)',?/\1/g")
+    VERSION=$(npm version | grep @ | sed -re "s/\{ '.*': '(.*)',?/\1/g")
     BRANCH_NAME=$(echo $TRAVIS_PULL_REQUEST_BRANCH | sed "s/[/]/_/g")
     TIMESTAMP=$(date +"%s")
     echo $VERSION-$BRANCH_NAME-$TIMESTAMP
@@ -10,7 +10,7 @@ if ! [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "|       Deploying on npm registry          |"
     echo "--------------------------------------------"
     npm version $VERSION-$BRANCH_NAME-$TIMESTAMP
-    [[ "$?" == 0 ]] && echo "npm version: Succeed" || echo "npm version: Failed"
+    [[ "$?" == 0 ]] && echo "npm version: Succeed" || (echo "npm version: Failed" && exit 1)
     npm publish --tag pull-request
-    [[ "$?" == 0 ]] && echo "npm publish: Succeed" || echo "npm publish: Failed"
+    [[ "$?" == 0 ]] && echo "npm publish: Succeed" || (echo "npm publish: Failed" && exit 2)
 fi
