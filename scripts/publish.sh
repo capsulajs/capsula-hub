8331a5ae4f8c38c50c34508b38d5ef9fba928b59
MikeSquall <michael.arbib@om2.com>
2019-02-11 17:13:07 +0200

    Use Travis to deploy & handle publishing develop

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -e
+
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+MSG_VERSION_SUCCESS="npm version: Succeed"
+MSG_VERSION_FAIL="npm version: Failed"
+MSG_PUBLISH_SUCCESS="npm publish: Succeed"
+MSG_PUBLISH_FAIL="npm publish: Failed"
+
+VERSION=$(npm version | grep @ | sed -re "s/\{ '.*': '(.*)',?/\1/g")
 
 if ! [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    VERSION=$(npm version | grep @ | sed -re "s/\{ '.*': '(.*)',?/\1/g")
     BRANCH_NAME=$(echo $TRAVIS_PULL_REQUEST_BRANCH | sed "s/[/]/-/g")
     TIMESTAMP=$(date +"%s")
     echo $VERSION-$BRANCH_NAME-$TIMESTAMP
@@ -10,9 +17,35 @@ if ! [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "|       Deploying on npm registry          |"
     echo "--------------------------------------------"
     npm version $VERSION-$BRANCH_NAME-$TIMESTAMP
-    [[ "$?" == 0 ]] && echo "npm version: Succeed" || (echo "npm version: Failed" && exit 1)
-    npm publish --tag pull-request
-    [[ "$?" == 0 ]] && echo "npm publish: Succeed" || (echo "npm publish: Failed" && exit 2)
+    if [[ "$?" == 0 ]]; then
+        echo $MSG_VERSION_SUCCESS
+    else
+        echo $MSG_VERSION_FAIL && exit 1
+    fi
+    npm publish --tag snapshot
+    if [[ "$?" == 0 ]]; then
+        echo $MSG_PUBLISH_SUCCESS
+    else
+        echo $MSG_PUBLISH_FAIL && exit 2
+    fi
+elif [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+    echo $VERSION
+    echo "--------------------------------------------"
+    echo "|     Deploying latest on npm registry     |"
+    echo "--------------------------------------------"
+    npm version patch
+    npm version $VERSION-$BRANCH_NAME-$TIMESTAMP
+    if [[ "$?" == 0 ]]; then
+        echo $MSG_VERSION_SUCCESS
+    else
+        echo $MSG_VERSION_FAIL && exit 1
+    fi
+    npm publish --access public
+    if [[ "$?" == 0 ]]; then
+        echo $MSG_PUBLISH_SUCCESS
+    else
+        echo $MSG_PUBLISH_FAIL && exit 2
+    fi
 else
     echo "*************************************************"
     echo "*   Not a pull request, npm publish skipped !   *"
