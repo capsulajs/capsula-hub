536d296db0eddf2784f03a9bca2f3417bbf5b6f4
MikeSquall <michael.arbib@om2.com>
2019-02-12 14:27:22 +0200

    test bash script with travis deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,6 +9,10 @@ MSG_PUBLISH_FAIL="npm publish: Failed"
 
 VERSION=$(npm version | grep @ | sed -re "s/\{ '.*': '(.*)',?/\1/g")
 
+echo "travis_branch           -> $TRAVIS_BRANCH"
+echo "travis_pr_branch        -> $TRAVIS_PULL_REQUEST_BRANCH"
+echo "travis_build_stage_name -> $TRAVIS_BUILD_STAGE_NAME"
+
 if [[ "$TRAVIS_BRANCH" =~ ^feature\/.*$ ]]; then
     BRANCH_NAME=$(echo $TRAVIS_PULL_REQUEST_BRANCH | sed "s/[/]/-/g")
     TIMESTAMP=$(date +"%s")
@@ -34,7 +38,6 @@ elif [[ "$TRAVIS_BRANCH" == "develop" ]]; then
     echo "|     Deploying latest on npm registry     |"
     echo "--------------------------------------------"
     npm version patch
-    npm version $VERSION-$BRANCH_NAME-$TIMESTAMP
     if [[ "$?" == 0 ]]; then
         echo $MSG_VERSION_SUCCESS
     else
