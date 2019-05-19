4e5cb27b5202789363c3df1aaf3b03422e6f797d
MikeSquall <michael.arbib@om2.com>
2019-05-19 19:26:02 +0300

    Added config and import the workspace

--- src/cli.js
+++ src/cli.js
@@ -4,23 +4,27 @@ const program = require('commander');
 
 program
   .version('0.0.1')
-  .command('run')
-  .description('Run a Capsula-Hub instance')
-  .action(() => {
+  .command('run <token>')
+  .description('Run a Capsula-Hub instance with a token')
+  .action((token) => {
     const app = require('express')();
     const Bundler = require('parcel-bundler');
     const Path = require('path');
+    const fs = require('fs');
 
-    const entryFiles = Path.join(__dirname, './index.html');
-    const options = {
-      outDir: '../dist',
-      outFile: 'index.html'
-    };
-    const bundler = new Bundler(entryFiles, options);
+    fs.writeFile('capsulahub.json', `{ "token": ${JSON.stringify(token)} }`, (response) => {
+      console.log(response);
+      const entryFiles = Path.join(__dirname, './index.html');
+      const options = {
+        outDir: '../dist',
+        outFile: 'index.html'
+      };
+      const bundler = new Bundler(entryFiles, options);
 
-    app.use(bundler.middleware());
+      app.use(bundler.middleware());
 
-    app.listen(3000);
+      app.listen(3000);
+    });
   });
 
 program.parse(process.argv);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import capsulahub from '../capsulahub.json';
 
 console.log('Hello from tsx!');
+console.log(capsulahub.token);
 
 ReactDOM.render(
   <div>
