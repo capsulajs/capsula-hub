9646990aba33c73f77fc6142214daee89229f172
MikeSquall <michael.arbib@om2.com>
2019-05-20 16:11:33 +0300

    Put the logic back in ts file

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,12 @@
 
 const program = require('commander');
 
+program.version('0.0.1');
+
 program
-  .version('0.0.1')
   .command('run <token>')
   .description('Run a Capsula-Hub instance with a token')
+  .option('-l, --local', 'Run with local configuration file')
   .action((token) => {
     const app = require('express')();
     const Bundler = require('parcel-bundler');
@@ -17,7 +19,7 @@ program
       const entryFiles = Path.join(__dirname, './index.html');
       const options = {
         outDir: '../dist',
-        outFile: 'index.html'
+        outFile: 'index.html?token=toto'
       };
       const bundler = new Bundler(entryFiles, options);
 
@@ -25,6 +27,34 @@ program
 
       app.listen(3000);
     });
+  });
+
+program
+  .command('config <path>')
+  .description('Serve the local config file for CapsulaHub')
+  .action((path) => {
+    console.log(process.cwd());
+    const express = require('express');
+    const workspaceConfig = require(`${process.cwd()}/${path}`);
+
+    const app = express();
+    const port = 3001;
+
+    const allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+    };
+
+    app.use(allowCrossDomain);
+    app.use(express.static('public'));
+
+    app.get('/configuration/workspace', (req, res) => {
+      res.send(workspaceConfig);
+    });
+
+    app.listen(port, () => console.log(`CapsulaHUB configuration listening on port ${port}!`));
   });
 
 program.parse(process.argv);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import { WorkspaceFactory as workspace } from 'https://capsulajs.s3.amazonaws.com/core/capsulahub-core-workspace/PR/feature/implement-workspace-factory/src/index.js';
 import capsulahub from '../capsulahub.json';
 
-console.log('Hello from tsx!');
-console.log(capsulahub.token);
+console.log(`Hello from tsx!\nMy token is ${capsulahub.token}`);
+// workspace.createWorkspace({ token: capsulahub.token}).then(() => console.log(`Workspace loaded !`));
+
 
 ReactDOM.render(
   <div>
