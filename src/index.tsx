bdef32bd2b21111ff0cb16a9cd3284a222e818b3
MikeSquall <michael.arbib@om2.com>
2019-05-20 19:16:14 +0300

    Added dependency to workspace

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { WorkspaceFactory as workspace } from 'https://capsulajs.s3.amazonaws.com/core/capsulahub-core-workspace/PR/feature/implement-workspace-factory/src/index.js';
+import { WorkspaceFactory } from '@capsulajs/capsulahub-core-workspace/lib/src';
 import capsulahub from '../capsulahub.json';
 
 console.log(`Hello from tsx!\nMy token is ${capsulahub.token}`);
-// workspace.createWorkspace({ token: capsulahub.token}).then(() => console.log(`Workspace loaded !`));
+const workspaceFactory = new WorkspaceFactory();
+workspaceFactory.createWorkspace({ token: capsulahub.token})
+  .then(() => console.log(`Workspace loaded !`))
+  .catch((err: Error) => console.error(err));
 
 
 ReactDOM.render(
