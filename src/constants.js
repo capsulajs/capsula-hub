e5296a449a098809b5220db086e140ac77891bb8
MikeSquall <michael.arbib@om2.com>
2019-05-22 16:18:06 +0300

    Set correct cdn path

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const CONSTANTS = {
   localConfigPort: 3001,
-  workspaceService: 'https://capsulajs.s3.amazonaws.com/core/capsulahub-core-workspace/PR/feature/implement-workspace-factory/index.js'
+  workspaceService: 'core/capsulahub-core-workspace/develop/index.js'
 };
 
 module.exports = CONSTANTS;
